e8938ff97c027f9fc4c5f1943dd827d0964fa035
gelaco1989 <gelaco1989@gmail.com>
2025-01-26 22:29:27 -0300

    atualização

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
@@ -6,16 +7,36 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
-import GridMenu from '../components/GridMenu/GridMenu';
-import './page.css';
-import Navbar from '../components/navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import GridMenu from "../components/GridMenu/GridMenu";
+import "./page.css";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
+  const [bannerImages, setBannerImages] = useState([]);
+
+  useEffect(() => {
+    const updateImages = () => {
+      if (window.innerWidth <= 768) {
+        // Imagens para mobile
+        setBannerImages(["/banner4.png", "/banner5.png", "/banner6.png"]);
+      } else {
+        // Imagens para web
+        setBannerImages(["/banner1.png", "/banner2.png", "/banner3.png"]);
+      }
+    };
+
+    updateImages(); // Chama ao carregar a página
+    window.addEventListener("resize", updateImages); // Atualiza ao redimensionar a janela
+
+    return () => {
+      window.removeEventListener("resize", updateImages); // Remove o listener ao desmontar
+    };
+  }, []);
+
   return (
-    <div className='container'>
-      <div className='containerSlide'>
+    <div className="container">
+      <div className="containerSlide">
         <Navbar />
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
@@ -25,28 +46,15 @@ const Home = () => {
           loop
           className="mySwiper"
         >
-          <SwiperSlide>
-            <img
-              className="imgBanner"
-              src="banner1.png"
-              alt="Caminhão 1"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="imgBanner"
-              src="/banner2.png"
-              alt="Caminhão 2"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="imgBanner"
-              src="/banner3.png"
-              alt="Caminhão 3"
-            />
-          </SwiperSlide>
-          
+          {bannerImages.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img
+                className="imgBanner"
+                src={image}
+                alt={`Banner ${index + 1}`}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
       <div className="containerForm">
