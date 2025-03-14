73f9ec6ad2a40dd16fd6a3133255b9caee73c704
gelaco1989 <gelaco1989@gmail.com>
2025-03-14 19:43:18 -0300

    atualização

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -1,25 +1,23 @@
 "use client"
 
-import Navbar from "@/components/navbar/Navbar";
 import './styles.css';
 
 export default function FormularioSerrafrete() {
   
 
   return (<>
-    <Navbar />
     <div className="containerMotoPage">
 
       <img
-        src="/logo.png"
+        src="/logoLanches.png"
         alt="Caminhão de frete"
         className="motoImg"
       />
       <h1>CONTATO</h1>
       <p><strong>WhatsApp: </strong> 21977384132</p>
-      <p><strong>Instagram: </strong>  @serrafrete</p>
-      <p><strong>Fã Page: </strong> Serra Frete</p>
-      <p><strong>Email: </strong>serrafrete@gmail.com</p>
+      <p><strong>Instagram: </strong>  @Lanches</p>
+      <p><strong>Fã Page: </strong> Lanches</p>
+      <p><strong>Email: </strong>Lanches@gmail.com</p>
       <p><strong>Localização: </strong>São pedro, Teresópolis, Rj</p>
      
      
--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -1,35 +1,29 @@
 "use client"
 
-import Navbar from "@/components/navbar/Navbar";
 import './styles.css';
 
 export default function FormularioSerrafrete() {
-  
+
 
   return (<>
-    <Navbar />
     <div className="containerMotoPage">
 
       <img
-        src="/sobre.png"
+        src="/logoLanches.png"
         alt="Caminhão de frete"
         className="motoImg"
       />
-      <h1>Compromisso e Qualidade em Cada Frete.</h1>
-      <p> Serrafrete nasceu de um sonho que começou em um balcão 
-        de vendas. Foi ali, ouvindo histórias e desafios de cada 
-        cliente, que percebemos uma necessidade real: encontrar 
-        fretes confiáveis, feitos com cuidado e compromisso. 
-        Inspirados por essas conversas e pela vontade de fazer a 
-        diferença, decidimos criar mais do que uma solução de 
-        transporte. Criamos uma ponte entre quem precisa e quem 
-        entrega, com a certeza de que cada frete carrega não só 
-        cargas, mas também confiança e dedicação. Aqui, cada 
-        quilômetro percorrido tem um propósito: superar 
-        expectativas e levar qualidade aonde for preciso.</p>
-      
-     
-     
+      <h1>Compromisso e Qualidade</h1>
+      <p>o lugar perfeito para matar sua fome com sabor e qualidade! Aqui, combinamos ingredientes frescos, receitas irresistíveis e um ambiente descontraído para tornar sua experiência única.
+
+        Nosso cardápio é recheado de opções deliciosas, desde hambúrgueres artesanais suculentos até porções e bebidas que complementam cada mordida. Seja para um lanche rápido, um encontro com amigos ou aquele pedido especial para matar a vontade, estamos sempre prontos para servir o melhor.
+
+        Na Malibu Lanches, prezamos pelo atendimento de primeira e pela satisfação dos nossos clientes. Cada detalhe, do preparo ao serviço, é pensado com carinho para garantir que você tenha uma experiência incrível.
+
+        Venha nos visitar ou faça seu pedido online e descubra por que somos a melhor escolha quando o assunto é lanche bom de verdade!</p>
+
+
+
     </div></>
   )
 }
