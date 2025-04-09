1755f510baca02cbc97ed9104856c5dbc5e6d76f
notebook <gelaco1989@gmail.com>
2025-04-08 23:28:48 -0300

    atualização

--- src/components/MenuLateral/menuLateral.js
+++ src/components/MenuLateral/menuLateral.js
@@ -1,16 +1,21 @@
+"use client";
+
 import React, { useState } from 'react';
-import Link from "next/link"; 
+import Link from "next/link";
+import { usePathname } from "next/navigation"; // IMPORTANTE!
 import "./styles.css";
 
 const MenuLateral = () => {
-  const [menuAberto, setMenuAberto] = useState(true); // Estado para controlar o menu
+  const [menuAberto, setMenuAberto] = useState(true);
+  const pathname = usePathname();
+
+  // Extrai o nome da lanchonete da URL
+  const pathParts = pathname.split("/");
+  const nomeLanchonete = pathParts[2] || "lanchonete-default"; // Ajusta conforme o path
 
   return (
     <div className={`menu-lateral ${menuAberto ? "aberto" : "fechado"}`}>
-      <button
-        className="botao-menu"
-        onClick={() => setMenuAberto(!menuAberto)}
-      >
+      <button className="botao-menu" onClick={() => setMenuAberto(!menuAberto)}>
         {menuAberto ? "◄" : "►"}
       </button>
 
@@ -19,13 +24,13 @@ const MenuLateral = () => {
           <h2>Menu</h2>
           <ul>
             <li>
-            <Link href="/admin/pedidos" style={{ textDecoration: "none", color: "inherit" }}> Pedidos  </Link>
+              <Link href={`/admin/${nomeLanchonete}/pedidos`} style={{ textDecoration: "none", color: "inherit" }}>Pedidos</Link>
             </li>
             <li>
-              <Link href="/admin/produtos" style={{ textDecoration: "none", color: "inherit" }}>Produtos</Link>
+              <Link href={`/admin/${nomeLanchonete}/produtos`} style={{ textDecoration: "none", color: "inherit" }}>Produtos</Link>
             </li>
             <li>
-              <Link href="/admin/config" style={{ textDecoration: "none", color: "inherit" }}>Configurações</Link>
+              <Link href={`/admin/${nomeLanchonete}/config`} style={{ textDecoration: "none", color: "inherit" }}>Configurações</Link>
             </li>
           </ul>
         </div>
